b2ed3edcc658d6a44c7af16d019c93a3852b62a7
Bryanf20 <fongangbryan@gmail.com>
2023-02-16 15:30:54 -0800

    fin

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 /*
- * Write a C program that prints exactly 
- * \"Programming is like building a multilingual puzzle, followed by a new line.
- *
  * main -entry point of program
  *
  * Return: zero if error,non zero if no error
